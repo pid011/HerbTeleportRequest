11c501dbcd8f1ccd82470d74bdce8bd45441a211
Herb9 <pksxtm@gmail.com>
2017-02-09 02:00:27 +0900

    Added command class (tpa, tpaccept).

---
+++ HerbTeleportRequest/HerbTeleportRequest/Command/TPACommand.cs
@@ -0,0 +1,23 @@
+﻿
+/*
+    ooooo   ooooo                     .o8        .ooooo.   
+    `888'   `888'                    "888       888' `Y88. 
+     888     888   .ooooo.  oooo d8b  888oooo.  888    888 
+     888ooooo888  d88' `88b `888""8P  d88' `88b  `Vbood888 
+     888     888  888ooo888  888      888   888       888' 
+     888     888  888    .o  888      888   888     .88P'  
+    o888o   o888o `Y8bod8P' d888b     `Y8bod8P'   .oP'     
+
+    Directed by Herb9.
+*/
+
+using System;
+
+namespace HerbTeleportRequest.Command
+{
+
+    class TpaCommand : HerbTeleportRequest
+    {
+
+    }
+}
